0396e924ea244b3abe5603fb285fdfb6ef36acdb
Tislander1 <blurryprojection@gmail.com>
2025-08-06 22:04:29 -0600

    add 2 to a list in ctypes

--- add_two.c
+++ add_two.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+// Function to add 2.0 to a float number
 float add_two(float number) {
     return number + 2.0;
+}
+
+
+// Function to add 2.0 to each element of a double array
+double* add_two_array(double* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] += 2.0;
+    }
+    return arr; // Return the pointer to the modified array
 }
 
 // compile add_two.c to a dll file with mingw:
--- add_two_lib.py
+++ add_two_lib.py
@@ -2,10 +2,29 @@
 
 import ctypes
 
-add_two_lib = ctypes.CDLL('./add_two.dll')
-add_two_lib.add_two.argtypes = [ctypes.c_float] # specify argument types
-add_two_lib.add_two.restype = ctypes.c_float  # specify result type
+# Load the shared library (DLL)
+twolib = ctypes.CDLL('./add_two.dll')
 
-result = add_two_lib.add_two(3.5)
-
+# use the float version of the function
+my_float = 3.5
+twolib.add_two.argtypes = [ctypes.c_float] # specify argument types
+twolib.add_two.restype = ctypes.c_float  # specify result type
+result = twolib.add_two(my_float) # call the function
 print(f"The result of adding two to 3.5 is: {result}")
+
+
+# use the list version of the function
+my_list = [1.0, 4.0, 5.9]
+twolib.add_two_array.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.c_int]  # specify argument types
+twolib.add_two_array.restype = ctypes.POINTER(ctypes.c_double)  #
+# Convert the Python list to a ctypes array of doubles
+c_array = (ctypes.c_double * len(my_list))(*my_list)
+# Call the function
+result_array = twolib.add_two_array(c_array, len(my_list))
+# Convert the result back to a Python list
+result_list = [result_array[i] for i in range(len(my_list))]
+print(f"The result of adding two to each element of {my_list} is: {result_list}")
+x = 2
+
+
+x = 2
